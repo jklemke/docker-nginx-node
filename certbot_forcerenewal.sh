257dace01a20736a8da4eaf86f67d43f6b213c95
Debian <admin@ip-10-181-4-56.eu-west-1.compute.internal>
2020-11-09 18:57:51 +0000

    fixed certbot service in docker-compose.yml; fixed cerbot shell scripts; fixed volume names in docker-compose.yml

---
+++ certbot_forcerenewal.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+COMPOSE="/usr/local/bin/docker-compose --no-ansi"
+DOCKER="/usr/bin/docker"
+
+cd ~/docker-nginx-node/
+$COMPOSE run certbot certonly --cert-name therapeutichermeneutic.org --webroot --webroot-path=/var/www/therapeutichermeneutic.org/html  && $COMPOSE kill -s SIGHUP webserver
+
--- certbot_renew.sh
+++ certbot_renew.sh
@@ -4,6 +4,5 @@ COMPOSE="/usr/local/bin/docker-compose --no-ansi"
 DOCKER="/usr/bin/docker"
 
 cd ~/docker-nginx-node/
-$COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP webserver
-$DOCKER system prune -af
+$COMPOSE run certbot renew --dry-run --cert-name therapeutichermeneutic.org --webroot --webroot-path=/var/www/therapeutichermeneutic.org/html  && $COMPOSE kill -s SIGHUP webserver
 
